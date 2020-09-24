ef2f463088a8c89d0dbb113998a5141bffa77d38
David Velasco <david.velasco@icf.unam.mx>
2020-09-24 19:31:29 +0200

    Included option for GPU-GPU comms, though they still don't work.

--- setups/fargo_multifluid/collisions.c
+++ setups/fargo_multifluid/collisions.c
@@ -24,10 +24,12 @@ void Collisions(real dt, int option) {
   */
  
   MULTIFLUID(if(Fluidtype == GAS) copy_field(Total_Density, Density));
-
+#ifdef MPICUDA
+  MPI_Bcast(Total_Density->field_gpu, Nx*(Ny+2*NGHY)*(Nz+2*NGHZ), MPI_DOUBLE, 0, FluidsComm);
+#else
   INPUT(Total_Density);
   MPI_Bcast(Total_Density->field_cpu, Nx*(Ny+2*NGHY)*(Nz+2*NGHZ), MPI_DOUBLE, 0, FluidsComm);
-
+#endif
   // Centered drag coefficient pre-factor
   FARGO_SAFE(ComputeDragCoeff());
   //--------------------------------------------------------------------------------------
@@ -35,32 +37,52 @@ void Collisions(real dt, int option) {
   Reset_field(Slope);
   //First step: Compute the constant C and B for each fluids  
   MULTIFLUID(ComputeCBcollisions_c(dt)); // local C and local B (x, y, z)
+#ifdef MPICUDA
+  MPI_Iallreduce(MPI_IN_PLACE, Slope->field_gpu, Nx*(Ny+2*NGHY)*(Nz+2*NGHZ),
+		 MPI_DOUBLE, MPI_SUM, FluidsComm, &request_c); // obtain C
+#else
   INPUT(Slope);
   MPI_Iallreduce(MPI_IN_PLACE, Slope->field_cpu, Nx*(Ny+2*NGHY)*(Nz+2*NGHZ),
 		 MPI_DOUBLE, MPI_SUM, FluidsComm, &request_c); // obtain C
-
+#endif
 
   //Second step: Compute the total C and B values (sum for all fluids)
 #ifdef X
   Reset_field(Mpx);
   MULTIFLUID(ComputeCBcollisions_cv(dt,0)); // local B (x)
+#ifdef MPICUDA
+  MPI_Iallreduce(MPI_IN_PLACE, Mpx->field_gpu, Nx*(Ny+2*NGHY)*(Nz+2*NGHZ),
+		 MPI_DOUBLE, MPI_SUM, FluidsComm, &request_x); // obtain B (x)
+#else
+
   INPUT(Mpx);
   MPI_Iallreduce(MPI_IN_PLACE, Mpx->field_cpu, Nx*(Ny+2*NGHY)*(Nz+2*NGHZ),
 		 MPI_DOUBLE, MPI_SUM, FluidsComm, &request_x); // obtain B (x)
 #endif
+#endif
 #ifdef Y  
   Reset_field(Mpy);
   MULTIFLUID(ComputeCBcollisions_cv(dt,1)); // local B (y)
+#ifdef MPICUDA
+  MPI_Iallreduce(MPI_IN_PLACE, Mpy->field_gpu, Nx*(Ny+2*NGHY)*(Nz+2*NGHZ),
+		 MPI_DOUBLE, MPI_SUM, FluidsComm, &request_y); // obtain B (y)
+#else
   INPUT(Mpy);
   MPI_Iallreduce(MPI_IN_PLACE, Mpy->field_cpu, Nx*(Ny+2*NGHY)*(Nz+2*NGHZ),
 		 MPI_DOUBLE, MPI_SUM, FluidsComm, &request_y); // obtain B (y)  
 #endif
+#endif
 #ifdef Z  
   Reset_field_cpu (Mpz);
   MULTIFLUID(ComputeCBcollisions_cv(dt,2)); // local B (z)
+#ifdef MPICUDA
+  MPI_Iallreduce(MPI_IN_PLACE, Mpz->field_gpu, Nx*(Ny+2*NGHY)*(Nz+2*NGHZ),
+		 MPI_DOUBLE, MPI_SUM, FluidsComm, &request_z); // obtain B (z)
+#else
   INPUT(Mpz);
   MPI_Iallreduce(MPI_IN_PLACE, Mpz->field_cpu, Nx*(Ny+2*NGHY)*(Nz+2*NGHZ),
 		 MPI_DOUBLE, MPI_SUM, FluidsComm, &request_z); // obtain B (z)    
+#endif
 #endif
  
   MPI_Wait(&request_c, MPI_STATUS_IGNORE);

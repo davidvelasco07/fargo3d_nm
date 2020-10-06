91eb3d80a9cabcf91966d1c7947a1d60b247adcc
Pablo Benitez Llambay <pbllambay@gmail.com>
2020-10-06 09:50:16 +0200

    WriteMerging() should work now.

--- src/output.c
+++ src/output.c
@@ -290,32 +290,32 @@ void WriteMerging(Field *f, int n) {
   INPUT(f);
 
   FILE *fo;
-  int i,j,k,m,jj;
   char outname[MAXLINELENGTH];
-  int next, previous;
   int relay;
-
+  int i, j, k;
+  
   sprintf(outname, "%s%s%d_grid%d.dat", OUTPUTDIR, f->name, n, Current_Jupiter_Patch->parent);
 
-  if (CPU_Rank > 0) { // Force sequential read
+  if (CPU_Rank > 0) // Force sequential write
     MPI_Recv (&relay, 1, MPI_INT, CPU_Rank-1, 42, DomainComm, MPI_STATUS_IGNORE);
-  }
 
   if (CPU_Master) fo = fopen(outname, "w");
-  else fo = fopen(outname, "a+");
-  
-  for (k=0; k<NZ; k++) {
-    for (j = 0; j<Ncpu_x; j++) {
-      if ((J==j) && (k>=Z0) && (k<(Z0+Nz))) {
-	      for (jj = NGHY; jj < Ny+NGHY; jj++)
-	        fwrite(f->field_cpu+(k-Z0+NGHZ)*Stride+jj*(Nx+2*NGHX)+NGHX, sizeof(real)*Nx, 1, fo);
-      }
+  else            fo = fopen(outname, "r+");
+
+  long offset = (Nx+2*NGHX)*Y0 + Nx*NY*Z0;
+
+  for (k=NGHZ; k<Nz+NGHZ; k++) {
+    fseek(fo, offset*sizeof(real), SEEK_SET);
+    for (j = NGHY; j < Ny+NGHY; j++) {
+      fwrite(f->field_cpu+k*Stride+j*(Nx+2*NGHX)+NGHX, sizeof(real)*Nx, 1, fo);
     }
+    offset += (Nx+2*NGHX)*NY;
   }
+
   fclose(fo);
-  if (CPU_Rank < CPU_Number-1) {  // Force sequential read
+  
+  if (CPU_Rank < CPU_Number-1)  // Force sequential write
     MPI_Send (&relay, 1, MPI_INT, CPU_Rank+1, 42, DomainComm);
-  }
   
 }
 
